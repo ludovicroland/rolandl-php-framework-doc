bba65a3f9f048b4e05211555dc14e219d622d941
rolandl <ludovic.c.roland@gmail.com>
2014-11-16 20:33:35 +0100

    Orthographe

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <h1>A propos</h1>
+          <h1>Qu'est-ce que c'est ?</h1>
           <p>Rolandl PHP Framework est un framework PHP Open-Source basé sur le tutoriel "<a href="http://openclassrooms.com/courses/programmez-en-oriente-objet-en-php" target="_blank">Programmez en orienté objet en PHP</a>" du site OpenClassrooms et étroitement lié au Framework CSS <a hre="http://getbootstrap.com/" target="_blank">Bootstrap</a>.</p>
           </p>Initialement créé pour le développement de mes propres projets clients, le code source est disponible sur Github depuis 2014 sous la license GNU GPL v2.0</p>
           <p>Bon développement !</p>
@@ -34,7 +34,7 @@
         </div>
        <div class="col-md-3 col-sm-6">
           <h3><i class="fa fa-shield"></i> Un framework sécurisé</h3>
-          <div>Ce framework fournit tous les outils nécesaire à la sécurisation de application web, de l'injection SQL à la validation des formulaires !</div>
+          <div>Ce framework fournit tous les outils nécesaire à la sécurisation de votre application web, de l'injection SQL à la validation des formulaires !</div>
         </div>
         <div class="col-md-3 col-sm-6">
           <h3><i class="fa fa-arrows-v"></i> Un framework extensible</h3>
@@ -56,7 +56,7 @@
           <div>Grâce à son système de routes, vos URL n'ont jamais été aussi belles !</div>
         </div>
         <div class="col-md-3 col-sm-6">
-          <h3><i class="fa fa-heart"></i> Fais avec amour</h3>
+          <h3><i class="fa fa-heart"></i> Fait avec amour</h3>
           <div>Ce framework a été développé avec amour, ce qui en fait le meilleur framework à mes yeux :)</div>
         </div>
       </div>
